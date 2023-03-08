ff4560e512e9df55239ae2ff95337dddb9e6b3f1
Lizeth Tancara <liztancarasirpa@gmail.com>
2023-03-08 16:51:09 -0300

    Agregando ejercicio 3 usando métodos de array

---
+++ modulo3-JS/unidad 4/ejercicio3/ejercicio3.js
@@ -0,0 +1,27 @@
+//Ejercicio 3:
+//Dado el siguiente array de alumnos utilizar los métodos de array vistos para generar un array con los alumnos aprobados. Se considera aprobado todo alumno que tenga una nota mayor o igual a 7.
+
+/*var alumnos = [
+  {nombre: "Juan Gomez",nota: 7,},
+  {nombre: "Pedro Rodriguez",nota: 4,},
+  {nombre: "Roxana García",nota: 8,},
+  {nombre: "Luciano Lopez",nota: 5,},
+  {nombre: "Fernanda Gimenez",nota: 6,},
+  {nombre: "Florencia Martinez",nota: 10,},
+  {nombre: "Raul Sanchez",nota: 7,},
+  {nombre: "Sandra Figueroa",nota: 8,},
+];*/
+
+var alumnos = [
+  { nombre: "Juan Gomez", nota: 7 },
+  { nombre: "Pedro Rodriguez", nota: 4 },
+  { nombre: "Roxana García", nota: 8 },
+  { nombre: "Luciano Lopez", nota: 5 },
+  { nombre: "Fernanda Gimenez", nota: 6 },
+  { nombre: "Florencia Martinez", nota: 10 },
+  { nombre: "Raul Sanchez", nota: 7 },
+  { nombre: "Sandra Figueroa", nota: 8 },
+];
+
+let aprobados = alumnos.filter((alumno) => alumno.nota >= 7);
+console.log(aprobados);
